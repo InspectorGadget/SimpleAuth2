0d78a2ce9b32818ad5e67bc82ea1552050853ca2
jazzwhistle <jdcraftmcpe@gmail.com>
2016-11-05 17:45:13 +0100

    Update for 0.16

--- src/SimpleAuth/SimpleAuth.php
+++ src/SimpleAuth/SimpleAuth.php
@@ -104,8 +104,9 @@ class SimpleAuth extends PluginBase {
         }
 
         $this->provider->updatePlayer($player, $player->getUniqueId(), $player->getAddress(), time());
+        $player->recalculatePermissions();
         $player->sendMessage(TextFormat::GREEN . $this->getMessage("login.success"));
-
+        
         $this->getMessageTask()->removePlayer($player);
 
         unset($this->blockSessions[$player->getAddress() . ":" . strtolower($player->getName())]);
